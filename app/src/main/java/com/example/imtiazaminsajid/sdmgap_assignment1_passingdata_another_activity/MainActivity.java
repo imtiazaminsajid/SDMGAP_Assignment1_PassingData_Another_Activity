b21ee386e176e99c4ea742229f2cfee43dfca388
Imtiaz Amin Sajid <imtiazaminsajid@gmail.com>
2018-07-26 22:38:27 +0600

    Initial commit

---
+++ app/src/main/java/com/example/imtiazaminsajid/sdmgap_assignment1_passingdata_another_activity/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.imtiazaminsajid.sdmgap_assignment1_passingdata_another_activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
